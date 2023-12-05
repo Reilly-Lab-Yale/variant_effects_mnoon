31eaec5f9057c1ff7a78628fd07c202d5848c5a2
Mackenzie Noon <mcn26@r813u09n01.mccleary.ycrc.yale.edu>
2023-12-05 18:19:57 -0500

    futzing with the annotation script, trying to get it to run in reasonable amounts of time. smaller run on chr22 so i have some data to play with and optimize subsequent steps. most work on 'intuition' analysis : see notebook there for documentation.

--- 1.annotate/wrapper_chr22.sh
+++ 1.annotate/wrapper_chr22.sh
@@ -2,11 +2,11 @@
 #SBATCH -J anno
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
-#SBATCH --mem=1991G
-#SBATCH -t 2-00:00:00
+#SBATCH --mem=600G
+#SBATCH -t 1-00:00:00
 #SBATCH -p ycga_bigmem
 
 
 module load miniconda
 conda activate mcn_vareff
-jupyter nbconvert --execute --to notebook --inplace ANNOTATE.ipynb
+jupyter nbconvert --execute --to notebook --inplace ANNOTATE_chr22.ipynb
