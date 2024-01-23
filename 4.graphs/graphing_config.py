e67d7587628a6eb74468d2b62beec12f2b5654a3
Mackenzie Noon <mackenziecnoon@gmail.com>
2024-01-23 17:20:31 -0500

    broke up the big graphing notebook some more, creating separate files for different tasks. minor fix to pleio

--- 4.graphs/graphing_config.py
+++ 4.graphs/graphing_config.py
@@ -3,11 +3,13 @@ genome_regions=["is_in_PLS","is_in_dELS","is_in_pELS","ALL"]
 data_base_path="/home/mcn26/varef/scripts/noon_data/3.count/"
 
 ###convert the boolean "in CADD category" cutoff to "mosst strict CADD cutoff row is counting". 
-CADD_cata = ["CADD>=10","CADD>=20","CADD>=30","CADD>=40","CADD>=50"][::-1]
+CADD_order = ["CADD>=10","CADD>=20","CADD>=30","CADD>=40","CADD>=50"][::-1]
 # We reverse the list so we check from right to left : from most to least strict
 #picking the strictest category to call as our 
 
 CADD_default='CADD<10'
+
+rarity_order = ["SINGLETON", "ULTRARARE", "RARE", "LOW_FREQ", "COMMON"]
 
 # Function to find the column with True value
 def find_true_column(row, columns,default):
