8980d737751653be0809897e539762f93fda2447
Mackenzie Noon <mackenziecnoon@gmail.com>
2024-02-06 15:50:19 -0500

    completed (I hope) preprocessing step for creating pleiotropy VS allele frequency box-plots

---
+++ 4.graphs/pleio_vs_purif_boxplot/wrapper.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH -J preprocess
+#SBATCH --output=preprocess_%A_%a.out
+#SBATCH --error=preprocess_%A_%a.err
+#SBATCH --cpus-per-task=15
+#SBATCH --array=0-15
+#SBATCH --mem-per-cpu=2G
+#SBATCH -t 2-00:00:00
+#SBATCH -p ycga
+
+module load miniconda
+conda activate mcn_vareff
+
+
+# Execute the converted Python script
+spark-submit \
+    --driver-memory 2g \
+    --executor-memory 12g \
+    --num-executors 2 \
+    --executor-cores 6 \
+    --conf spark.executor.memoryOverhead=2g \
+    preprocess.py
