21add980692259028660fa0bfb3d65fa3187c679
Mackenzie Noon <mackenziecnoon@gmail.com>
2024-04-15 14:27:56 -0400

    running the last three chromosomes

--- 2.0.annotate/wrapper.sh
+++ 2.0.annotate/wrapper.sh
@@ -2,11 +2,11 @@
 #SBATCH -J annotate
 #SBATCH --output=anno_%A_%a.out
 #SBATCH --error=anno_%A_%a.err
-#SBATCH --array=1-18
+#SBATCH --array=1-3
 #SBATCH --cpus-per-task=2
-#SBATCH --mem=100G
-#SBATCH -t 2-00:00:00
-#SBATCH -p ycga
+#SBATCH --mem=15G
+#SBATCH -t 7-00:00:00
+#SBATCH -p ycga_long
 #SBATCH --mail-user mackenzie.noon@yale.edu
 #SBATCH --mail-type=END,FAIL
 
@@ -19,4 +19,4 @@ export which_chr="chr${SLURM_ARRAY_TASK_ID}"
 
 
 # Execute the converted Python script
-spark-submit --executor-memory 90g --driver-memory 9g ANNOTATE.py
+spark-submit --executor-memory 10g --driver-memory 5g ANNOTATE.py
