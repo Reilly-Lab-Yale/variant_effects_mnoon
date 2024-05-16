57055a64cd152aad07c7e63c933116b7ff98eec9
Mackenzie Noon <mackenziecnoon@gmail.com>
2024-05-16 16:24:15 -0400

    stipulated subset of chromosomes & added notebook -> script conversion

--- 3.0.pleio_and_filter/wrapper.sh
+++ 3.0.pleio_and_filter/wrapper.sh
@@ -2,7 +2,7 @@
 #SBATCH -J pleio
 #SBATCH --output=filter_%A_%a.out
 #SBATCH --error=filter_%A_%a.err
-#SBATCH --array=1-22
+#SBATCH --array=9,13-22
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=6G
 #SBATCH -t 24:00:00
@@ -12,6 +12,8 @@ module load miniconda
 conda activate mcn_varef
 
 export which_chr="chr${SLURM_ARRAY_TASK_ID}"
+
+jupyter nbconvert --to script pleio_and_filter.ipynb
 
 
 # Execute the converted Python script
