7ea297c41a3ab82f4ec64ee13dcdc2e28f8c9a40
Mackenzie Noon <mackenziecnoon@gmail.com>
2024-05-16 20:19:07 -0400

    autoconverts notebook to script & restricted chromosomes

--- 3.6.remove_non_snp/wrapper.sh
+++ 3.6.remove_non_snp/wrapper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --array=1-22
+#SBATCH --array=9,13-22
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=16G
 #SBATCH -t 2-00:00:00
@@ -11,6 +11,8 @@
 module load miniconda
 conda activate mcn_varef
 
+
+jupyter nbconvert --to script remove_non_snp.ipynb
 
 export which_chr="chr${SLURM_ARRAY_TASK_ID}"
 
