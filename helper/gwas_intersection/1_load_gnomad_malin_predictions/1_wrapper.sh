5b5f81547a8b15cdf4dd739fb3e6c5e1f4cd49d5
Mackenzie Noon <mackenziecnoon@gmail.com>
2025-02-10 15:06:07 -0500

    re-added audo export

--- helper/gwas_intersection/1_load_gnomad_malin_predictions/1_wrapper.sh
+++ helper/gwas_intersection/1_load_gnomad_malin_predictions/1_wrapper.sh
@@ -5,6 +5,6 @@
 #SBATCH --mem-per-cpu=10G
 module load miniconda
 conda activate mcn_varef
-# run `jupyter nbconvert --to script 2_load_gnomad_malin_predictions.ipynb`
+jupyter nbconvert --to script 1_load_gnomad_malin_predictions.ipynb
 # to convert notebook to script
-ipython 2_load_gnomad_malin_predictions.py
+ipython 1_load_gnomad_malin_predictions.py
