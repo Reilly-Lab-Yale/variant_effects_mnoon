dfc8885e5c42b63c79ab7e59499596a8423e08c1
Mackenzie Noon <mackenziecnoon@gmail.com>
2025-02-10 15:08:26 -0500

    adding dump to fall back on in case of db corruption

---
+++ helper/gwas_intersection/1_load_gnomad_malin_predictions/dump.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH -p ycga_long
+#SBATCH -t 4-00:00:00
+#SBATCH -c 2
+#SBATCH --mem-per-cpu=5G
+module load miniconda
+conda activate mcn_varef
+
+postgres -D /home/mcn26/palmer_scratch/db -p 5433 &
+echo '[+] Waiting 120s for server to start'
+sleep 120
+echo '[+] Beginning dump'
+pg_dump "postgresql://mr_root:password@localhost:5433/scratch" -f /home/mcn26/palmer_scratch/malin_loaded_backup_file.sql
+echo '[+] Dump complete! Shutting down server.'
+pg_ctl -D /home/mcn26/palmer_scratch/db stop &
+echo '[+] Waiting 120s for server to stop.'
+sleep 120
+conda deactivate
