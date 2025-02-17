53f24c05b271aa87e085140ed29322780b687ec4
Mackenzie Noon <mackenziecnoon@gmail.com>
2025-02-17 16:23:39 -0500

    moved dump to dump a different step

--- helper/gwas_intersection/dump.sh
+++ helper/gwas_intersection/dump.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -p ycga_long
-#SBATCH -t 4-00:00:00
+#SBATCH -t 8-00:00:00
 #SBATCH -c 2
 #SBATCH --mem-per-cpu=5G
 module load miniconda
@@ -10,12 +10,12 @@ postgres -D /home/mcn26/palmer_scratch/db -p 5433 &
 echo '[+] Waiting 120s for server to start'
 sleep 120
 echo '[+] Beginning dump'
-pg_dump "postgresql://mr_root:password@localhost:5433/scratch" -f /home/mcn26/palmer_scratch/post_intersection_dump.sql
+pg_dump "postgresql://mr_root:password@localhost:5433/scratch" -f /home/mcn26/palmer_scratch/post_load_broken_table.sql
 echo '[+] Dump complete! Shutting down server.'
 pg_ctl -D /home/mcn26/palmer_scratch/db stop &
 echo '[+] Waiting 120s for server to stop.'
 sleep 120
 echo '[+] Zipping dump'
-gzip /home/mcn26/palmer_scratch/post_intersection_dump.sql
+gzip /home/mcn26/palmer_scratch/post_load_broken_table.sql
 echo '[+] Done zipping!'
 conda deactivate
