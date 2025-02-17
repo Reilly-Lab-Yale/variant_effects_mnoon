7e2407f3294d8e91528c927bf6b6e8933d89a16d
Mackenzie Noon <mackenziecnoon@gmail.com>
2025-02-17 14:51:57 -0500

    moved dump script to dump a different step

--- helper/gwas_intersection/06_intersect/dump.sh
+++ helper/gwas_intersection/06_intersect/dump.sh
@@ -10,12 +10,12 @@ postgres -D /home/mcn26/palmer_scratch/db -p 5433 &
 echo '[+] Waiting 120s for server to start'
 sleep 120
 echo '[+] Beginning dump'
-pg_dump "postgresql://mr_root:password@localhost:5433/scratch" -f /home/mcn26/palmer_scratch/post_indexing_dump.sql
+pg_dump "postgresql://mr_root:password@localhost:5433/scratch" -f /home/mcn26/palmer_scratch/post_intersection_dump.sql
 echo '[+] Dump complete! Shutting down server.'
 pg_ctl -D /home/mcn26/palmer_scratch/db stop &
 echo '[+] Waiting 120s for server to stop.'
 sleep 120
 echo '[+] Zipping dump'
-gzip /home/mcn26/palmer_scratch/post_indexing_dump.sql
+gzip /home/mcn26/palmer_scratch/post_intersection_dump.sql
 echo '[+] Done zipping!'
 conda deactivate
