438b273966bdbec799884333dea88a24ab3a0a91
Mackenzie Noon <mackenziecnoon@gmail.com>
2025-04-01 10:43:36 -0400

    restoring later step

--- helper/gwas_intersection/restore.sh
+++ helper/gwas_intersection/restore.sh
@@ -2,7 +2,10 @@
 #SBATCH -c 1
 #SBATCH -p ycga
 #SBATCH --mem-per-cpu=8G
-#SBATCH -t 4:00:00
+#SBATCH -t 8:00:00
+
+#echo "[+] unziping backup"
+#gunzip /home/mcn26/palmer_scratch/post_load_broken_table.sql.gz
 echo "[+] loading env"
 module load miniconda
 conda activate mcn_varef
@@ -11,7 +14,7 @@ postgres -D /home/mcn26/palmer_scratch/db -p 5433 &
 echo "[+] waiting 20s for server to start"
 sleep 20
 echo "[+] restoring"
-psql postgresql://mr_root:password@localhost:5433/scratch -f /home/mcn26/palmer_scratch/post_indexing_dump.sql
+psql postgresql://mr_root:password@localhost:5433/scratch -f /home/mcn26/palmer_scratch/post_load_broken_table.sql
 echo "[+] DONE. Killing server.."
 pg_ctl -D ~/palmer_scratch/db stop
 sleep 10
