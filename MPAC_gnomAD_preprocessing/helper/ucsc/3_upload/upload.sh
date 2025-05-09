c12e7fcbdfc4f7ab9ddbd556dd8acc2ecc3471e8
Mackenzie Noon <mackenziecnoon@gmail.com>
2025-05-09 12:41:41 -0400

    +upload script

---
+++ MPAC_gnomAD_preprocessing/helper/ucsc/3_upload/upload.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -p ycga
+#SBATCH --mem-per-cpu=1G
+#SBATCH --cpus-per-task=8
+#SBATCH -t 2:00:00
+#ran aws configure
+module load awscli
+export aws
+run() {
+  echo "[+] $*"
+  "$@"
+}
+bucket="s3://vcf-mpac-test/"
+run aws s3 sync /vast/palmer/pi/reilly/VariantEffects/data/ucsc/sorted ${bucket}/bcf
+run aws s3 sync /vast/palmer/pi/reilly/VariantEffects/data/ucsc/bw ${bucket}/bw
