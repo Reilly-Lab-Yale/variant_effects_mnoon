78a75e5098557d4fdb8ba6dbd4d04c21c77a8a2d
Mackenzie Noon <mackenziecnoon@gmail.com>
2025-05-08 20:03:46 -0400

    re-run hepg2

--- MPAC_gnomAD_preprocessing/helper/ucsc/0_break_preds/break_preds.sh
+++ MPAC_gnomAD_preprocessing/helper/ucsc/0_break_preds/break_preds.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --array=0-21
-#SBATCH -t 8:00:00
+#SBATCH -t 2:00:00
 
 module load BCFtools/1.21-GCC-12.2.0
 export bcftools
@@ -24,7 +24,7 @@ inp=${data_root}/${chromosomes[${SLURM_ARRAY_TASK_ID}]}
 chr_name=$(echo ${chromosomes[${SLURM_ARRAY_TASK_ID}]} | awk -F. -v n="7" '{print $n}')
 
 #for cell_type in HepG2 K562 SKNSH
-for cell_type in K562
+for cell_type in HepG2
 do
 	#run echo ${cell_type}
 	run bcftools annotate -x ^INFO/${cell_type}__ref,^INFO/${cell_type}__alt,^INFO/${cell_type}__skew ${inp} -o ${outputdir}/${cell_type}/${chr_name}.vcf
