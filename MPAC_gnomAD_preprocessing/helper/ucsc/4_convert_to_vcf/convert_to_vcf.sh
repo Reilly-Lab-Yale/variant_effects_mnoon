41871bdfa129ba7a99ae72c2fbdfd44a90c01619
Mackenzie Noon <mackenziecnoon@gmail.com>
2025-05-12 16:41:33 -0400

    + working wigs

---
+++ MPAC_gnomAD_preprocessing/helper/ucsc/4_convert_to_vcf/convert_to_vcf.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH -p ycga
+#SBATCH -t 3:00:00
+#SBATCH -c 4
+#SBATCH --mem-per-cpu=5G
+#SBATCH --array 0-2
+
+module load BCFtools
+
+
+root="/vast/palmer/pi/reilly/VariantEffects/data/ucsc"
+
+
+
+cell_types=(K562 HepG2 SKNSH)
+cell_type=${cell_types[$SLURM_ARRAY_TASK_ID]}
+
+bcftools view ${root}/combined/${cell_type}.bcf -Oz | bgzip > ${root}/vcf/${cell_type}.vcf.gz
+tabix ${root}/vcf/${cell_type}.vcf.gz
