ba4202161dd80b01f320db99f918c46566e5f3ce
Mackenzie Noon <mackenziecnoon@gmail.com>
2025-05-12 15:09:48 -0400

    manual stanzas

--- MPAC_gnomAD_preprocessing/helper/ucsc/5_upload_textfiles.sh
+++ MPAC_gnomAD_preprocessing/helper/ucsc/5_upload_textfiles.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-#SBATCH -p ycga
-#SBATCH --mem-per-cpu=1G
-#SBATCH --cpus-per-task=8
-#SBATCH -t 2:00:00
-#ran aws configure
 module load awscli
 export aws
 run() {
@@ -13,12 +8,7 @@ run() {
 bucket="s3://vcf-mpac-test"
 
 #files in the root path
-run aws s3 cp hub.txt $bucket
-run aws s3 cp genomes.txt $bucket
-run aws s3 cp trackDb.txt $bucket
+run aws s3 cp ucsc_hub_textfiles/hub.txt $bucket
+run aws s3 cp ucsc_hub_textfiles/genomes.txt $bucket
+run aws s3 cp ucsc_hub_textfiles/trackDb.txt $bucket
 
-for cell_type in HepG2 K562 SKNSH
-do
-	#copy bcf tracklist
-	run aws s3 cp trackList.txt ${bucket}/bcf/${cell_type}/trackList.txt
-done
