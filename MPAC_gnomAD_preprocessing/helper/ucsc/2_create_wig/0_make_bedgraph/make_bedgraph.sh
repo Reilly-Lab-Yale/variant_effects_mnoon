c25d498cc974795d6329ae27ac231cd1408dd306
Mackenzie Noon <mackenziecnoon@gmail.com>
2025-05-13 15:14:31 -0400

    + dir fix

--- MPAC_gnomAD_preprocessing/helper/ucsc/2_create_wig/0_make_bedgraph/make_bedgraph.sh
+++ MPAC_gnomAD_preprocessing/helper/ucsc/2_create_wig/0_make_bedgraph/make_bedgraph.sh
@@ -7,7 +7,7 @@
 
 module load BCFtools/1.21-GCC-12.2.0
 
-data_root="/home/mcn26/varef/data/ucsc/sorted"
+data_root="/home/mcn26/varef/data/ucsc/broken"
 output_dir="/home/mcn26/varef/data/ucsc/bed"
 
 #get the file names
@@ -20,6 +20,7 @@ for cell_type in HepG2 K562 SKNSH
 do
 	inp=${data_root}/${cell_type}/${chromosomes[${SLURM_ARRAY_TASK_ID}]}
 	chr_name=$(echo ${chromosomes[${SLURM_ARRAY_TASK_ID}]} | awk -F. -v n="1" '{print $n}')
+	mkdir -p ${output_dir}/${cell_type}
 	out=${output_dir}/${cell_type}/${chr_name}.bedgraph
 	echo "processing ${inp}, spitting to ${out}"
 	bcftools annotate -x ^INFO/${cell_type}__skew ${inp} \
