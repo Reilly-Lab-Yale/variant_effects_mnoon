b454addc5c328f255bc9f6e2b4eaf87afe406a19
Mackenzie Noon <mackenziecnoon@gmail.com>
2025-05-13 14:53:30 -0400

    fixed paths

--- MPAC_gnomAD_preprocessing/helper/ucsc/0_break_preds/break_preds.sh
+++ MPAC_gnomAD_preprocessing/helper/ucsc/0_break_preds/break_preds.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --array=0-21
-#SBATCH -t 2:00:00
+#SBATCH -t 0:30:00
 
 module load BCFtools/1.21-GCC-12.2.0
 export bcftools
@@ -22,13 +22,11 @@ chromosomes=($(ls -1 ${data_root} | grep converted | grep -v tbi))
 inp=${data_root}/${chromosomes[${SLURM_ARRAY_TASK_ID}]}
 chr_name=$(echo ${chromosomes[${SLURM_ARRAY_TASK_ID}]} | awk -F. -v n="7" '{print $n}')
 
-outputdir="/home/mcn26/varef/data/ucsc/broken/${cell_type}"
-mkdir -p ${outputdir}
-
-#for cell_type in HepG2 K562 SKNSH
-for cell_type in HepG2
+for cell_type in HepG2 K562 SKNSH
 do
-	run bcftools annotate -x ^INFO/${cell_type}__ref,^INFO/${cell_type}__alt,^INFO/${cell_type}__skew ${inp} -o ${outputdir}${chr_name}.vcf
+	outputdir="/home/mcn26/varef/data/ucsc/broken/${cell_type}"
+	mkdir -p ${outputdir}
+	run bcftools annotate -x ^INFO/${cell_type}__ref,^INFO/${cell_type}__alt,^INFO/${cell_type}__skew ${inp} -o ${outputdir}/${chr_name}.vcf
 done
 
 echo "[+] done."
