eb89e0eaef752825ca7814565848f5c1bda50f62
Mackenzie Noon <mackenziecnoon@gmail.com>
2025-05-13 16:10:20 -0400

    +parallel

--- MPAC_gnomAD_preprocessing/helper/ucsc/3_combine/combine_vcf.sh
+++ MPAC_gnomAD_preprocessing/helper/ucsc/3_combine/combine_vcf.sh
@@ -3,29 +3,29 @@
 #SBATCH -t 8:00:00
 #SBATCH -c 4
 #SBATCH --mem=4G
+#SBATCH --array 0-2
 set -euo pipefail
 module load BCFtools/1.21-GCC-12.2.0
 
 # CONFIG
-input_root="/vast/palmer/pi/reilly/VariantEffects/data/ucsc/sorted"
-output_dir="/vast/palmer/pi/reilly/VariantEffects/data/ucsc/combined"
+input_root="/vast/palmer/pi/reilly/VariantEffects/data/ucsc/broken"
+output_dir="/vast/palmer/pi/reilly/VariantEffects/data/ucsc/combined_vcf"
 cell_types=("HepG2" "K562" "SKNSH")
 
 mkdir -p "$output_dir"
 
-for cell in "${cell_types[@]}"; do
-    indir="${input_root}/${cell}"
-    outfile="${output_dir}/${cell}.bcf"
+cell=${cell_types[$SLURM_ARRAY_TASK_ID]}
 
-    echo "[+] Processing $cell..."
+indir="${input_root}/${cell}"
+outfile="${output_dir}/${cell}.vcf.gz"
 
-    # Natural sort and list BCFs
-    files=$(ls "${indir}"/chr*.vcf.gz | sort -V)  # -V = natural sort (handles chr10 correctly)
+echo "[+] Processing $cell..."
 
-    # Concatenate and write output
-    bcftools concat -Ob -o "$outfile" $files
+# Natural sort and list VCFs
+files=$(ls "${indir}"/chr*.vcf | sort -V)  # -V = natural sort (e.g. handles chr10 vs chr1 correctly)
 
-    # Index the result
-    bcftools index -f "$outfile"
+# Concatenate and write output
+bcftools concat -Ob -o "$outfile" $files
 
-done
+# Index the result
+bcftools index -t "$outfile"
