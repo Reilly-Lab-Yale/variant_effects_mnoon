f900d750cd50752886dd2329837a408d9fbbd7d0
Mackenzie Noon <mackenziecnoon@gmail.com>
2025-06-02 14:01:58 -0400

    +bb convertion step

--- MPAC_gnomAD_preprocessing/helper/ucsc/3_convert_to_bigbed/convert_to_bb.sh
+++ MPAC_gnomAD_preprocessing/helper/ucsc/3_convert_to_bigbed/convert_to_bb.sh
@@ -1,1 +1,21 @@
-bedToBigBed -as=specification.as -type=bed9+2 test.bed hg38.chrom.sizes test.bb
+#!/bin/bash
+#SBATCH -p ycga
+#SBATCH -t 8:00:00
+#SBATCH -c 4
+#SBATCH --mem=16G
+cell_types=("HepG2" "K562" "SKNSH")
+
+module reset
+module load miniconda
+conda activate mcn_varef
+
+for cell in "${cell_types[@]}"; do
+    echo "[+] Processing $cell..."
+    #make the specification autosql files
+    echo "table SKNSH_MPAC_gnomAd" > ${cell}.as
+    echo "\"MPAC variant effect predictions for all gnomad variants in cell line ${cell}\"" >> ${cell}.as
+    cat specification_columns.txt >> ${cell}.as
+    #now convert the bed file to bigbed
+    bedToBigBed -as=${cell}.as -type=bed9+4 /home/mcn26/varef/data/ucsc/pre_bigbed_bed/${cell}.bed hg38.chrom.sizes /home/mcn26/varef/data/ucsc/bigbed/${cell}.bb
+    
+done
