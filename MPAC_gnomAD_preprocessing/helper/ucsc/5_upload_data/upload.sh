895ed47fbd959f7eb36cd164547c574a9993aa13
Mackenzie Noon <mackenziecnoon@gmail.com>
2025-06-02 14:22:38 -0400

    +upload bigbed

--- MPAC_gnomAD_preprocessing/helper/ucsc/5_upload_data/upload.sh
+++ MPAC_gnomAD_preprocessing/helper/ucsc/5_upload_data/upload.sh
@@ -11,6 +11,7 @@ run() {
   "$@"
 }
 bucket="s3://vcf-mpac-test"
-run aws s3 cp /home/mcn26/varef/scripts/noon_scripts/MPAC_gnomAD_preprocessing/helper/ucsc/test.bb ${bucket}
+run aws s3 sync /vast/palmer/pi/reilly/VariantEffects/data/ucsc/bigbed ${bucket}/bigbed
+#run aws s3 cp /home/mcn26/varef/scripts/noon_scripts/MPAC_gnomAD_preprocessing/helper/ucsc/test.bb ${bucket}
 #run aws s3 sync /vast/palmer/pi/reilly/VariantEffects/data/ucsc/combined_vcf ${bucket}/vcf
 #run aws s3 sync /vast/palmer/pi/reilly/VariantEffects/data/ucsc/combined_bigwig ${bucket}/bw
