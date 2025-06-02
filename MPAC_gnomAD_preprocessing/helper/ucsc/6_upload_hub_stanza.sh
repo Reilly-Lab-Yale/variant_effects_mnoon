36eb8c2e2ce649a1316e5c10e2bc327de8a92c10
Mackenzie Noon <mackenziecnoon@gmail.com>
2025-06-02 17:22:50 -0400

    +better decription pages

--- MPAC_gnomAD_preprocessing/helper/ucsc/6_upload_hub_stanza.sh
+++ MPAC_gnomAD_preprocessing/helper/ucsc/6_upload_hub_stanza.sh
@@ -15,3 +15,4 @@ run aws s3 cp ucsc_hub_textfiles/trackDb.txt $bucket
 run aws s3 cp ucsc_hub_textfiles/SKNSH.html $bucket
 run aws s3 cp ucsc_hub_textfiles/K562.html $bucket
 run aws s3 cp ucsc_hub_textfiles/HepG2.html $bucket
+run aws s3 cp ucsc_hub_textfiles/ucsc_demo.png $bucket
