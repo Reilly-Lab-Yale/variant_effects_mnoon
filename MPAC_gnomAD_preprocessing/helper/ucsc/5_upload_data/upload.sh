83c5bf4b2416321b875836ffeff6ad6efd9a533a
Mackenzie Noon <mackenziecnoon@gmail.com>
2025-06-09 12:09:51 -0400

    ~gcloud upload

--- MPAC_gnomAD_preprocessing/helper/ucsc/5_upload_data/upload.sh
+++ MPAC_gnomAD_preprocessing/helper/ucsc/5_upload_data/upload.sh
@@ -5,14 +5,13 @@
 #SBATCH -t 2:00:00
 #ran aws configure
 module reset
-module load awscli
-export aws
+module load gcloud
 run() {
   echo "[+] $*"
   "$@"
 }
-bucket="s3://vcf-mpac-test"
-run aws s3 sync /vast/palmer/pi/reilly/VariantEffects/data/ucsc/bigbed ${bucket}/bigbed
+bucket="gs://mpac_ucsc_genome_browser"
+run gsutil -m rsync -r /vast/palmer/pi/reilly/VariantEffects/data/ucsc/bigbed ${bucket}/bigbed
 #run aws s3 cp /home/mcn26/varef/scripts/noon_scripts/MPAC_gnomAD_preprocessing/helper/ucsc/test.bb ${bucket}
 #run aws s3 sync /vast/palmer/pi/reilly/VariantEffects/data/ucsc/combined_vcf ${bucket}/vcf
 #run aws s3 sync /vast/palmer/pi/reilly/VariantEffects/data/ucsc/combined_bigwig ${bucket}/bw
